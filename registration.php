0a9ec1b80bafd3aac7c65b1a739703a0fe03ca3d
Your Name <mmdhudasia@gmail.com>
2020-07-30 10:50:17 +0530

    Initial release

---
+++ registration.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Solwin Infotech
+ * Solwin PromotionalBar Extension
+ *
+ * @category   Solwin
+ * @package    Solwin_PromotionalBar
+ * @copyright  Copyright © 2006-2019 Solwin (https://www.solwininfotech.com)
+ * @license    https://www.solwininfotech.com/magento-extension-license/
+ */
+?>
+<?php
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Solwin_PromotionalBar',
+    __DIR__
+);
